756001c3ea9d8cb252c182b71675c0e7aceebee2
Vlad <heisenbergjessy95@gmail.com>
2026-07-30 14:33:26 +0300

    refactor(server): refactor code

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,16 +2,28 @@ package server
 
 import (
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
 
 type Server struct {
+	upgrader websocket.Upgrader
 }
 
 func NewServer() *Server {
-	return &Server{}
+
+	return &Server{upgrader: websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+		EnableCompression: true,
+	}}
+
 }
 
 func (s *Server) GetRouter() *gin.Engine {
@@ -23,30 +35,86 @@ func (s *Server) GetRouter() *gin.Engine {
 
 func (s *Server) Run(c *gin.Context) {
 
-	var upgrader = websocket.Upgrader{
-		ReadBufferSize:  1024,
-		WriteBufferSize: 1024,
+	conn, err := s.upgrader.Upgrade(c.Writer, c.Request, nil)
+	if err != nil {
+		log.Println(err)
+		return
 	}
 
-	ws, err := upgrader.Upgrade(c.Writer, c.Request, nil)
-	if err != nil {
-		log.Fatal(err)
+	if err = conn.SetReadDeadline(time.Now().Add(time.Second * 30)); err != nil {
+		log.Println(err)
+		return
 	}
-	defer ws.Close()
+
+	handle(conn)
+}
+
+func handle(conn *websocket.Conn) {
+
+	msgChan := make(chan []byte)
+	defer close(msgChan)
+	defer conn.Close()
+
+	ticker := time.NewTicker(time.Second * 25)
+	defer ticker.Stop()
+
+	conn.SetPongHandler(func(string) error { return conn.SetReadDeadline(time.Now().Add(time.Second * 20)) })
+	if err := conn.SetReadDeadline(time.Now().Add(time.Second * 30)); err != nil {
+		log.Println(err)
+		return
+	}
+
+	go func() {
+
+		defer conn.Close()
+
+		for {
+			select {
+			case msg, ok := <-msgChan:
+
+				if !ok {
+					break
+				}
+
+				if err := conn.SetWriteDeadline(time.Now().Add(time.Second * 30)); err != nil {
+					log.Println(err)
+					break
+				}
+
+				if err := conn.WriteMessage(websocket.TextMessage, msg); err != nil {
+					log.Println(err)
+					break
+				}
+
+			case <-ticker.C:
+				if err := conn.SetWriteDeadline(time.Now().Add(time.Second * 30)); err != nil {
+					log.Println(err)
+					break
+				}
+
+				if err := conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+					log.Println(err)
+					break
+				}
+			}
+		}
+	}()
 
 	for {
 
-		messageType, message, err := ws.ReadMessage()
+		_, message, err := conn.ReadMessage()
 		if err != nil {
 			log.Println(err)
+			return
 		}
 
 		log.Printf("received: %s", message)
 
-		if err = ws.WriteMessage(messageType, message); err != nil {
+		msgChan <- message
+
+		if err = conn.SetReadDeadline(time.Now().Add(time.Second * 30)); err != nil {
 			log.Println(err)
+			return
 		}
-
 	}
-
 }
