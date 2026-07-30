945062f9380d88f8b74f4fcc62100d5d17fa156d
Vlad <heisenbergjessy95@gmail.com>
2026-07-30 15:50:21 +0300

    feat: add config file

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+var values map[string]string
+
+func InitConfig() {
+	var err error
+	values, err = godotenv.Read(".env")
+
+	if err != nil {
+		log.Fatalf("error to read config: %e", err)
+	}
+}
+
+func Port() string {
+	return values["PORT"]
+}
+
+func Host() string {
+	return values["HOST"]
+}
