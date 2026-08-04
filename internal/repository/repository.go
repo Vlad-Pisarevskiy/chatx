94afd349763bf08ab9126f5dbe906e3cb8ede099
Vlad <heisenbergjessy95@gmail.com>
2026-08-04 16:47:01 +0300

    feat: add repository layer:

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,28 @@
 package repository
+
+import (
+	"chatflow/internal/model"
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Repository struct {
+	pool *pgxpool.Pool
+}
+
+func NewRepository(ctx context.Context, pool *pgxpool.Pool) *Repository {
+
+	return &Repository{pool: pool}
+}
+
+func (r *Repository) RegisterUser(ctx context.Context, client model.Client) error {
+
+	_, err := r.pool.Exec(ctx, "INSERT INTO users(name, login, email, password) VALUES ($1, $2, $3, $4)",
+		client.Name, client.Login, client.Email, client.Password)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
