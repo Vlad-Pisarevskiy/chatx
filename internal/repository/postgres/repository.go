1e331390f6d5af9500a1caa209750c4474ecaa80
Vlad <heisenbergjessy95@gmail.com>
2026-08-20 17:30:30 +0300

    feat(repository): add chat functions

--- internal/repository/postgres/repository.go
+++ internal/repository/postgres/repository.go
@@ -15,6 +15,8 @@ type Repository struct {
 	pool *pgxpool.Pool
 }
 
+const zeroInt = 0
+
 func NewRepository(ctx context.Context, pool *pgxpool.Pool) *Repository {
 
 	return &Repository{pool: pool}
@@ -33,42 +35,76 @@ func (r *Repository) RegisterUser(ctx context.Context, client model.User) error 
 
 func (r *Repository) LoginExists(ctx context.Context, login string) (bool, error) {
 
-	row := r.pool.QueryRow(ctx, "SELECT login FROM users WHERE login = $1", login)
+	row := r.pool.QueryRow(ctx, "SELECT EXISTS(SELECT 1 FROM users WHERE login = $1)", login)
 
-	var dbLogin string
+	var exists bool
 
-	err := row.Scan(&dbLogin)
+	err := row.Scan(&exists)
 	if err != nil {
-		if errors.Is(err, pgx.ErrNoRows) {
-			return false, nil
-		}
-		return false, err
+		return exists, err
 	}
 
-	return true, nil
+	return exists, nil
 }
 
 func (r *Repository) FindUserByLogin(ctx context.Context, login string) (*model.User, error) {
 
-	var client model.User
+	var user model.User
 
 	row := r.pool.QueryRow(ctx, "SELECT id, name, login, password FROM users WHERE login=($1)", login)
-	err := row.Scan(
-		&client.ID,
-		&client.Name,
-		&client.Login,
-		&client.Password)
-	if err != nil {
+	if err := row.Scan(
+		&user.ID,
+		&user.Name,
+		&user.Login,
+		&user.Password); err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return nil, errors1.ErrIncorrectLoginData
 		}
 		return nil, err
 	}
 
-	return &client, nil
+	return &user, nil
 }
 
-func (r *Repository) AddToken(ctx context.Context, userID string, token []byte) error {
+func (r *Repository) FindUserByID(ctx context.Context, id int) (*model.UserFromDB, error) {
+
+	var user model.UserFromDB
+
+	row := r.pool.QueryRow(ctx, "SELECT id, name, login FROM users WHERE id=$1", id)
+	if err := row.Scan(
+		&user.ID,
+		&user.Name,
+		&user.Login); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, errors1.ErrIncorrectLoginData
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (r *Repository) GetUsersExcept(ctx context.Context, id int) ([]*model.UserFromDB, error) {
+
+	var users []*model.UserFromDB
+
+	rows, err := r.pool.Query(ctx, "SELECT id, name, login FROM users WHERE id <> $1", id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	users, err = scanRows(rows, users)
+	if err != nil {
+		if users != nil {
+			return users, nil
+		}
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (r *Repository) AddToken(ctx context.Context, userID int, token []byte) error {
 
 	_, err := r.pool.Exec(ctx, "INSERT INTO tokens(user_id, token_hash) VALUES ($1, $2)", userID, token)
 	if err != nil {
@@ -93,10 +129,9 @@ func (r *Repository) CheckToken(ctx context.Context, token []byte) (userID int, 
 	return userID, nil
 }
 
-func (r *Repository) GetUsers(ctx context.Context) ([]model.UserFromDB, error) {
+func (r *Repository) GetUsers(ctx context.Context) ([]*model.UserFromDB, error) {
 
-	var user model.UserFromDB
-	var users []model.UserFromDB
+	var users []*model.UserFromDB
 
 	rows, err := r.pool.Query(ctx, "SELECT id, name, login FROM users")
 	if err != nil {
@@ -104,12 +139,69 @@ func (r *Repository) GetUsers(ctx context.Context) ([]model.UserFromDB, error) {
 	}
 	defer rows.Close()
 
+	users, err = scanRows(rows, users)
+	if err != nil {
+		if users != nil {
+			return users, nil
+		}
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func (r *Repository) ChatExists(ctx context.Context, from int, to int) (bool, error) {
+
+	row := r.pool.QueryRow(ctx,
+		`SELECT EXISTS(SELECT 1 FROM chats c 
+    		JOIN users_chats u ON c.id = u.chat_id
+       		WHERE u.user_id IN ($1, $2))`, from, to)
+
+	var exists bool
+
+	if err := row.Scan(&exists); err != nil {
+		return exists, err
+	}
+
+	return exists, nil
+}
+
+func (r *Repository) StartChat(ctx context.Context, from int, to int) (int, error) {
+
+	tx, err := r.pool.Begin(ctx)
+	if err != nil {
+		return zeroInt, err
+	}
+	defer tx.Rollback(ctx)
+
+	var chatID int
+	if err = tx.QueryRow(ctx, `INSERT INTO chats DEFAULT VALUES RETURNING id`).Scan(&chatID); err != nil {
+		return zeroInt, err
+	}
+
+	_, err = tx.Exec(ctx, `INSERT INTO users_chats (chat_id, user_id) VALUES ($1, $2), ($1, $3)`, chatID, from, to)
+	if err != nil {
+		return zeroInt, err
+	}
+
+	return chatID, tx.Commit(ctx)
+}
+
+func scanRows(rows pgx.Rows, users []*model.UserFromDB) ([]*model.UserFromDB, error) {
+
 	for rows.Next() {
 
-		if err = rows.Scan(&user.ID, &user.Name, &user.Login); err != nil {
+		var user model.UserFromDB
+
+		if err := rows.Scan(
+			&user.ID,
+			&user.Name,
+			&user.Login); err != nil {
 			log.Println(err)
+			return nil, err
 		}
-		users = append(users, user)
+
+		users = append(users, &user)
 	}
 
 	if rows.Err() != nil {
