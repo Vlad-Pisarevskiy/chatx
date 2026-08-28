13dc91498ab2c3204d1326d674211aa117a86874
Vlad <heisenbergjessy95@gmail.com>
2026-08-28 17:32:24 +0300

    feat(server): add graceful shutdown

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -7,17 +7,38 @@ import (
 	server2 "chatflow/internal/server"
 	service2 "chatflow/internal/service"
 	"context"
+	"errors"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+const (
+	interval = time.Hour * 2
+	timeout  = time.Second * 10
+)
+
 func main() {
 
-	config.InitConfig()
-	pool, err := pgxpool.New(context.Background(), config.DBPath())
+	// Проблема: когда приходит сигнал от системы о завершении работы программы, мэйн его не ловит.
+	// Он смотрит только на внутренние ошибки. Чтобы поймать такой сигнал нам надо указать,
+	// специальный контекст нотифай который будет передавать управление сигналами от системы к этому контексту.
+	// Мы указываем набор сигналов при получении которых управление должно быть передано программе
+	// При получении такого сигнала вызывается Done метод
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
 
+	errCh := make(chan error, 1)
+
+	config.InitConfig()
+
+	pool, err := pgxpool.New(context.Background(), config.DBPath())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,10 +47,61 @@ func main() {
 	hub := hub2.New()
 	repository := postgres.New(pool)
 	service := service2.New(repository)
-	server := server2.New(service, hub)
 
+	go cleanupTokens(ctx, service)
+
+	server := server2.New(service, hub)
 	r := server.GetRouter()
-	if err = r.Run(fmt.Sprintf("%s:%s", config.Host(), config.Port())); err != nil {
-		log.Fatal(err)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf("%s:%s", config.Host(), config.Port()),
+		Handler: r,
+	}
+
+	go listenAndServe(srv, errCh)
+
+	select {
+	case <-ctx.Done():
+		stop()
+	case err = <-errCh:
+		log.Println(err)
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	if err = srv.Shutdown(shutdownCtx); err != nil {
+		log.Println(err)
+	}
+
+	hub.Close()
+}
+
+func cleanupTokens(ctx context.Context, service *service2.Service) {
+
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case <-ticker.C:
+			deleted, err := service.CleanupTokens(ctx)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			log.Printf("Deleted %d expired tokens", deleted)
+		}
+	}
+}
+
+func listenAndServe(server *http.Server, errCh chan error) {
+	if err := server.ListenAndServe(); err != nil {
+		if errors.Is(err, http.ErrServerClosed) {
+			return
+		}
+		errCh <- err
 	}
 }
