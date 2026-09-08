53de70ade3a7078d9659c81dff3d939c9676d080
Vlad <heisenbergjessy95@gmail.com>
2026-09-08 16:09:52 +0300

    refactor(model): remove message

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,20 +1,10 @@
 package model
 
-import (
-	"time"
-)
-
 type User struct {
 	ID       int
 	Name     string
 	Login    string
 	Password string
-}
-
-type Message struct {
-	From      int       `json:"from"`
-	Data      string    `json:"data"`
-	CreatedAt time.Time `json:"created_at"`
 }
 
 type UserFromDB struct {
